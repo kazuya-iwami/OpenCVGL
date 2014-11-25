9d6922e5f55172da74445b9fb81b8c1718f8ad69
Iwami kazuya <k.bluewing.i@gmail.com>
2014-11-25 15:46:54 +0900

    #1

--- OpenCVGL1.1/blackboard.cpp
+++ OpenCVGL1.1/blackboard.cpp
@@ -25,47 +25,74 @@ void detectLine(Mat &input,Mat &processed);
 int main (int argc, char **argv)
 {
     
+//    
+//    // 1. prepare VideoCapture Object
+//    VideoCapture cap;
+//    Mat frame_cap,frame_pro;
+//    std::string input_index;
+//    if(argc >= 2){ // capture from video file
+//        input_index = argv[1];
+//        cap.open(input_index);
+//    }else{ // capture from camera
+//        cap.open(0);
+//    }
+//    
+//    cvNamedWindow ("Capture", CV_WINDOW_AUTOSIZE);
+//    cvNamedWindow ("Project", CV_WINDOW_AUTOSIZE);
+//    
+//    if(!cap.isOpened()){
+//        printf("no input video\n");
+//        return 0;
+//    }
+//    else
+//    {
+//        bool loop_flag = true;
+//        while(loop_flag){
+//            cap >> frame_cap;
+//            if(frame_cap.empty()){
+//                break;
+//            }
+//            detectLine(frame_cap, frame_cap);
+//            imshow("Capture",frame_cap);
+//            imshow("Project",frame_cap);
+//            
+//            // 5. process according to input key
+//            int k = cvWaitKey(33);
+//            switch(k){
+//                case 'q':
+//                case 'Q':
+//                    loop_flag = false;
+//                    break;
+//            }
+//        }
+//    }
     
-    // 1. prepare VideoCapture Object
-    VideoCapture cap;
-    Mat frame_cap,frame_pro;
-    std::string input_index;
-    if(argc >= 2){ // capture from video file
-        input_index = argv[1];
-        cap.open(input_index);
-    }else{ // capture from camera
-        cap.open(0);
+    
+    cv::Mat input,processed;
+    const char *input_file;
+    const char* preset_file = "/Users/kazuya/Git/OpenCVGL/OpenCVGL1.1/figures2.jpg";
+    
+    if(argc==2){
+        input_file=argv[1];
+    }else{
+        input_file=preset_file;
     }
     
-    cvNamedWindow ("Capture", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow ("Project", CV_WINDOW_AUTOSIZE);
+    //2.read an image from the specified file
+    input=cv::imread(input_file,1);
+    if(input.empty()){
+        fprintf(stderr,"cannot open %s\n",input_file);
+        exit(0);
+    }
     
-    if(!cap.isOpened()){
-        printf("no input video\n");
-        return 0;
-    }
-    else
-    {
-        bool loop_flag = true;
-        while(loop_flag){
-            cap >> frame_cap;
-            if(frame_cap.empty()){
-                break;
-            }
-            detectLine(frame_cap, frame_cap);
-            imshow("Capture",frame_cap);
-            imshow("Project",frame_cap);
-            
-            // 5. process according to input key
-            int k = cvWaitKey(33);
-            switch(k){
-                case 'q':
-                case 'Q':
-                    loop_flag = false;
-                    break;
-            }
-        }
-    }
+    detectLine(input, processed);
+    
+    cv::namedWindow("processed image",1);
+
+    cv::imshow("processed image",processed);
+
+    cv::waitKey(0);
+
     
     
     
@@ -82,33 +109,28 @@ void detectLine(Mat &input,Mat &processed){
     cvtColor(input,canny_output,CV_BGR2GRAY);  //色空間の変換(グレイスケール化)
     blur(canny_output, canny_output, Size(5,5) );//平滑化して誤差を減らす
     Canny (canny_output, canny_output, 50,150, 3);//canny法によるエッジ検出
-    //cvThreshold(img_gray, img_gray, 0, 255, CV_THRESH_BINARY);//二値化
     
-    findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    
-    vector<vector<Point> >::iterator it;
-    for( it = contours.begin(); it != contours.end(); it++ )
-        cout << *it << endl;
-    
-    
-
-    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-    
-    for( int i=0; i < (int)contours.size(); i++ ) {
-        for(int j=0; j<(int)contours[i].size(); j++){
-            circle(drawing, contours[i][j], 10, Scalar(100,200,0),1);
-        }
-    }
-
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-    }
-    
+//    //cvThreshold(img_gray, img_gray, 0, 255, CV_THRESH_BINARY);//二値化
+//    
+//    findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+//
+//    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+//    
+//    for( int i=0; i < (int)contours.size(); i++ ) {
+//        for(int j=0; j<(int)contours[i].size(); j++){
+//            circle(drawing, contours[i][j], 10, Scalar(100,200,0),1);
+//        }
+//    }
+//
+//    for( int i = 0; i< contours.size(); i++ )
+//    {
+//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+//        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+//    }
+//    
     
     // (5)検出結果表示用のウィンドウを確保し表示する
-    drawing.copyTo(processed);
+    canny_output.copyTo(processed);
     
 
     
