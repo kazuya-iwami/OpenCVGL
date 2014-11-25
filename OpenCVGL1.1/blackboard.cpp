90ed4585febb65f70e11f3321cab10cf0815aa51
Iwami kazuya <k.bluewing.i@gmail.com>
2014-11-25 14:36:28 +0900

    直線認識中　findContoursにより輪郭認識　contourの中身表示

--- OpenCVGL1.1/blackboard.cpp
+++ OpenCVGL1.1/blackboard.cpp
@@ -15,16 +15,20 @@
 #include <imgproc.hpp>
 
 #include <math.h>
+#include <iostream>
 
-void detectLine(cv::Mat &input,cv::Mat &processed);
+using namespace cv;
+using namespace std;
+
+void detectLine(Mat &input,Mat &processed);
 
 int main (int argc, char **argv)
 {
     
     
     // 1. prepare VideoCapture Object
-    cv::VideoCapture cap;
-    cv::Mat frame;
+    VideoCapture cap;
+    Mat frame_cap,frame_pro;
     std::string input_index;
     if(argc >= 2){ // capture from video file
         input_index = argv[1];
@@ -34,6 +38,7 @@ int main (int argc, char **argv)
     }
     
     cvNamedWindow ("Capture", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow ("Project", CV_WINDOW_AUTOSIZE);
     
     if(!cap.isOpened()){
         printf("no input video\n");
@@ -43,12 +48,13 @@ int main (int argc, char **argv)
     {
         bool loop_flag = true;
         while(loop_flag){
-            cap >> frame;               
-            if(frame.empty()){
+            cap >> frame_cap;
+            if(frame_cap.empty()){
                 break;
             }
-            detectLine(frame, frame);
-            cv::imshow("Capture",frame);
+            detectLine(frame_cap, frame_cap);
+            imshow("Capture",frame_cap);
+            imshow("Project",frame_cap);
             
             // 5. process according to input key
             int k = cvWaitKey(33);
@@ -66,40 +72,49 @@ int main (int argc, char **argv)
     return 0;
 }
 
-void detectLine(cv::Mat &input,cv::Mat &processed){
+void detectLine(Mat &input,Mat &processed){
     
-    int i;
-    IplImage  *src_img_prob = 0, *src_img_gray = 0;
-    CvMemStorage *storage;
-    CvSeq *lines = 0;
-    CvPoint *point;
+    Mat canny_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    RNG rng(12345);
     
-    // (1)画像の読み込み
-    IplImage tmp = input;
-    src_img_prob = &tmp;
-    src_img_gray =cvCreateImage( cvGetSize(src_img_prob),IPL_DEPTH_8U,1); //1チャネル８ビットのIplImageを作成
-    cvCvtColor(src_img_prob, src_img_gray, CV_BGR2GRAY);  //色空間の変換(グレイスケール化)
+    cvtColor(input,canny_output,CV_BGR2GRAY);  //色空間の変換(グレイスケール化)
+    blur(canny_output, canny_output, Size(5,5) );//平滑化して誤差を減らす
+    Canny (canny_output, canny_output, 50,150, 3);//canny法によるエッジ検出
+    //cvThreshold(img_gray, img_gray, 0, 255, CV_THRESH_BINARY);//二値化
     
-    // (2)ハフ変換のための前処理
-    cvCanny (src_img_gray, src_img_gray, 50, 200, 3);
-    storage = cvCreateMemStorage (0);
+    findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
     
-    // (4)確率的ハフ変換による線分の検出と検出した線分の描画
-    lines = 0;
-    lines = cvHoughLines2 (src_img_gray, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI / 180, 50, 50, 10);
-    for (i = 0; i < lines->total; i++) {
-        point = (CvPoint *) cvGetSeqElem (lines, i);
-        cvLine (src_img_prob, point[0], point[1], CV_RGB (255, 0, 0), 3, 8, 0);
+    vector<vector<Point> >::iterator it;
+    for( it = contours.begin(); it != contours.end(); it++ )
+        cout << *it << endl;
+    
+    
+
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    
+    for( int i=0; i < (int)contours.size(); i++ ) {
+        for(int j=0; j<(int)contours[i].size(); j++){
+            circle(drawing, contours[i][j], 10, Scalar(100,200,0),1);
+        }
+    }
+
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
     }
     
-    // (5)検出結果表示用のウィンドウを確保し表示する
-    processed = cv::cvarrToMat(src_img_prob);
     
-    //cvReleaseImage (&src_img_prob);
-    //cvReleaseImage (&src_img_gray);
-    cvReleaseMemStorage (&storage);
+    // (5)検出結果表示用のウィンドウを確保し表示する
+    drawing.copyTo(processed);
+    
 
+    
 }
+
+
 
 
 
